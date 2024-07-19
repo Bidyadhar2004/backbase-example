b45e4043fd0ab76731546f6f14ec56a025bbbdd0
Madhusmita <info.smita2021@gmail.com>
2024-07-19 17:04:21 +0530

    COde Review Changes

--- src/test/java/com/backbase/example/accountservice/controller/AccountControllerTest.java
+++ src/test/java/com/backbase/example/accountservice/controller/AccountControllerTest.java
@@ -2,44 +2,37 @@ package com.backbase.example.accountservice.controller;
 
 import com.backbase.example.accountservice.dto.AccountDto;
 import com.backbase.example.accountservice.entity.Account;
-import com.backbase.example.accountservice.security.JwtFilter;
-import com.backbase.example.accountservice.security.JwtUtil;
-import com.backbase.example.accountservice.security.SecurityConfig;
+import com.backbase.example.accountservice.repository.AccountRepository;
 import com.backbase.example.accountservice.service.AccountService;
-import com.backbase.example.accountservice.service.TokenBlacklistService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@Disabled
-@ActiveProfiles("test")
-@SpringBootTest(classes = {SecurityConfig.class, TokenBlacklistService.class, JwtUtil.class, AccountController.class, JwtFilter.class})
-@Import(SecurityConfig.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AccountControllerTest {
 
     @Autowired
@@ -47,6 +40,9 @@ public class AccountControllerTest {
 
     @MockBean
     private AccountService accountService;
+
+    @MockBean
+    private AccountRepository accountRepository;
 
     @InjectMocks
     private AccountController accountController;
@@ -57,66 +53,105 @@ public class AccountControllerTest {
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(accountController).build();
     }
 
     @Test
+    @WithMockUser(username = "user", roles = {"USER"})
     public void testGetAllAccountsWithPagination() throws Exception {
         Account account1 = new Account(1L, "Account1", "123456789");
         Account account2 = new Account(2L, "Account2", "987654321");
-        Pageable pageable = PageRequest.of(0, 2);
-        Page<Account> accountPage = new PageImpl<>(Arrays.asList(account1, account2), pageable, 2);
 
-        when(accountService.getAllAccounts(anyInt(), anyInt())).thenReturn(accountPage);
+        List<Account> accounts = Arrays.asList(account1, account2);
+        Pageable pageable = PageRequest.of(0, 2, Sort.by("id").ascending());
+        Page<Account> accountPage = new PageImpl<>(accounts, pageable, accounts.size());
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/accounts")
+        Mockito.when(accountService.getAllAccounts(pageable)).thenReturn(accountPage);
+
+        Mockito.when(accountRepository.findAll(pageable)).thenReturn(accountPage);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/accounts")
                 .param("page", "0")
                 .param("size", "2")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name").value("Account1"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].name").value("Account2"));
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content.length()").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].accountName").value("Account1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].id").value(2L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].accountName").value("Account2"));
     }
 
     @Test
+    @WithMockUser(username = "user", roles = {"USER"})
     public void testGetAccountByNumber() throws Exception {
-        when(accountService.getAccountByAccountNumber(anyString())).thenReturn(Optional.of(new Account(1L, "Account1", "123456789")));
+        String accountNumber = "123456789";
+        Account account = new Account(1L, "Test Account", accountNumber);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/account/123456789"))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Account1"));
+        when(accountService.getAccountByAccountNumber(accountNumber)).thenReturn(Optional.of(account));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/account")
+                .param("accountNumber", accountNumber)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountName").value("Test Account"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").value(accountNumber));
     }
 
     @Test
+    @WithMockUser(username = "user", roles = {"USER"})
     public void testGetAccountByName() throws Exception {
-        when(accountService.getAccountByAccountNumber(anyString())).thenReturn(Optional.of(new Account(1L, "Account1", "123456789")));
+        String accountName = "Test Account";
+        Account account = new Account(1L, accountName, "123456789");
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/account/Account1"))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Account1"));
+        when(accountService.getAccountByAccountName(accountName)).thenReturn(Optional.of(account));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api//account")
+                .param("accountName", accountName)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountName").value(accountName))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").value("123456789"));
     }
 
     @Test
+    @WithMockUser(username = "user", roles = {"USER"})
     public void testCreateAccount() throws Exception {
-        Account account = new Account(1L, "Account1", "123456789");
-        when(accountService.createAccount(any(AccountDto.class))).thenReturn(account);
+        AccountDto accountDto = new AccountDto();
+        accountDto.setAccountName("New Account");
+        accountDto.setAccountNumber("123456789");
+        Account createdAccount = new Account(1L, accountDto.getAccountName(), accountDto.getAccountNumber());
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/accounts")
+        when(accountService.createAccount(accountDto)).thenReturn(createdAccount);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/accounts")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(account)))
-                .andExpect(status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Account1"));
+                .content(objectMapper.writeValueAsString(accountDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountName").value("New Account"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").value("123456789"));
     }
 
     @Test
+    @WithMockUser(username = "user", roles = {"USER"})
     public void testUpdateAccount() throws Exception {
-        Account account = new Account(1L, "UpdatedAccount", "123456789");
-        when(accountService.updateAccount(anyLong(), any(AccountDto.class))).thenReturn(Optional.of(account));
+        Long accountId = 1L;
+        AccountDto accountDto = new AccountDto();
+        accountDto.setAccountName("Updated Account");
+        accountDto.setAccountNumber("345123456");
+        Account updatedAccount = new Account(accountId, accountDto.getAccountName(), accountDto.getAccountNumber());
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/accounts/1")
+        when(accountService.updateAccount(accountId, accountDto)).thenReturn(Optional.of(updatedAccount));
+
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/accounts/{id}", accountId)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(account)))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("UpdatedAccount"));
+                .content(objectMapper.writeValueAsString(accountDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(accountId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountName").value("Updated Account"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber").value("345123456"));
     }
 }
